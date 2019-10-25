6c5bbfaae7ee83f5410f24f8d1603aef8f6ecb9e
Pietro Carrara <piticarrara@gmail.com>
2019-10-25 14:59:02 -0300

    Implement Goal

---
+++ Scenes/Goal/Goal.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+onready var area = $Area2D;
+
+func _ready():
+	area.connect("body_entered", self, "bodyEntered");
+
+func bodyEntered(b):
+	print('Wooow');
+	print(b.name);
+	if b.name == "Player" && !getWallColor().isBlack:
+		print('You Win!');
+
+func getWallColor():
+	var bodies = area.get_overlapping_bodies();
+	for body in bodies:
+		if body.name != "Player":
+			return body.get_node("ColorSwitcher");
+	return null;
