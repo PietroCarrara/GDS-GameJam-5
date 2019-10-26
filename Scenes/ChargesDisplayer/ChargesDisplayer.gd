a2796923a842cad71ed4e1569b0b3a35d24f9f9d
Pietro Carrara <piticarrara@gmail.com>
2019-10-26 07:10:22 -0300

    Implement charge counter with icons

--- Scenes/ChargesDisplayer/ChargesDisplayer.gd
+++ Scenes/ChargesDisplayer/ChargesDisplayer.gd
@@ -1,8 +1,25 @@
 extends Node2D
 
+export var icon: Texture;
+
 func _ready():
 	var player = get_parent().find_node("Player");
 	player.connect("ChargesChanged", self, "playerChargesChanged");
 
 func playerChargesChanged(n):
-	get_node("box").set_bbcode("Remaining Charges: [color=#f50]"+str(n)+"[/color]");
+	clearCharges();
+	for i in range(n):
+		addCharge();
+	
+func addCharge():
+	var img = TextureRect.new();
+	var size = Vector2(128, 229) * 0.25;
+	img.set_texture(icon);
+	img.set_custom_minimum_size(size);
+	img.set_size(size);
+	img.set_expand(true);
+	$HBoxContainer.add_child(img);
+
+func clearCharges():
+	for child in $HBoxContainer.get_children():
+    	child.queue_free();
